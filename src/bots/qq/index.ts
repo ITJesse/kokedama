caea23f59d94c08ee2880394cde16a197f414490
Jesse Zhu <jesse@itjesse.com>
2021-12-01 03:15:17 +0900

    add tg profile photo

--- src/bots/qq/index.ts
+++ src/bots/qq/index.ts
@@ -19,8 +19,19 @@ export function qqBot(bot: Telegraf) {
 
     const username = getName(ctx.message.from)
 
-    const message = `来自电报的消息\n---------------------------\n${username} 说：\n${ctx.message.text}`
-    await qq.sendImage('https://dummyimage.com/1x1/000/fff', message)
+    const { photos } = await bot.telegram.getUserProfilePhotos(
+      ctx.message.from.id,
+      0,
+      1,
+    )
+    const profilePhoto = photos[0]?.[0]?.file_id
+    let imageUrl = 'https://dummyimage.com/1x1/000/fff'
+    if (profilePhoto) {
+      const photoUrl = await bot.telegram.getFileLink(profilePhoto)
+      imageUrl = photoUrl.href
+    }
+    const message = `\n来自电报的消息\n---------------------------\n${username} 说：\n${ctx.message.text}`
+    await qq.sendImage(imageUrl, message)
     // await qq.sendMessage(message)
     // await redis.setex(
     //   `${QQ_MSG_TO_TG_PREFIX}${message_id}`,
