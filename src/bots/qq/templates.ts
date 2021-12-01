db12c81805a0145e835f91c535541e199b3ecaf3
Jesse Zhu <jesse@itjesse.com>
2021-12-02 02:15:34 +0900

    update qq message template

--- src/bots/qq/templates.ts
+++ src/bots/qq/templates.ts
@@ -6,7 +6,5 @@ export const buildQQMessage = ({
   profilePhoto: string
   username: string
   message: string
-}) => `来自电报的消息
----------------------------
-[CQ:image,file=${profilePhoto}]${username} 说：
+}) => `[CQ:image,file=${profilePhoto}]${username} 说：
 ${message}`
