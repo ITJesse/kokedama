dfd6a57c93049e2d0c348874f125f34b488e49d8
Jesse Zhu <jesse@itjesse.com>
2022-02-10 02:16:19 +0900

    multipart download

--- src/bots/waifu2x/worker.ts
+++ src/bots/waifu2x/worker.ts
@@ -2,12 +2,13 @@ import axios from 'axios'
 import { Worker } from 'bullmq'
 import { randomUUID } from 'crypto'
 import fs from 'fs'
+import MultipartDownload from 'multipart-download'
 import { execFile } from 'mz/child_process'
 import os from 'os'
 import path from 'path'
 import sharp from 'sharp'
 
-import { signUrl, store, uploadBuf } from '@/utils/oss'
+import { store, uploadBuf } from '@/utils/oss'
 
 import { connection } from './queue'
 
@@ -16,6 +17,27 @@ const binaryPath =
     ? '/Users/jesse/Github/waifu2x-converter-cpp/build/waifu2x-converter-cpp'
     : 'waifu2x-converter-cpp'
 
+const downloadImage = (imageUrl: string): Promise<Buffer> =>
+  new Promise((resolve, reject) => {
+    let dataLen = 0
+    const startTime = Date.now()
+    new MultipartDownload()
+      .start(imageUrl, {
+        numOfConnections: 5,
+        writeToBuffer: true,
+      })
+      .on('data', (data) => {
+        dataLen += data.length
+      })
+      .on('end', (data) => {
+        const duration = Date.now() - startTime
+        const speed = dataLen / 1024 / (duration / 1000)
+        console.log('speed:', speed.toFixed(1), 'kB/s')
+        resolve(data)
+      })
+      .on('error', (err) => reject(err))
+  })
+
 new Worker(
   'waifu2x',
   async (job) => {
@@ -23,18 +45,16 @@ new Worker(
     console.log('start waifu2x', imageUrl)
     const input = path.resolve('/tmp', randomUUID())
     const output = path.resolve('/tmp', `${randomUUID()}.png`)
+
     try {
-      const { data } = await axios.get(imageUrl, {
-        responseType: 'arraybuffer',
-        timeout: 30000,
-      })
-      fs.writeFileSync(input, Buffer.from(data))
+      const data = await downloadImage(imageUrl)
+      fs.writeFileSync(input, data)
     } catch (err) {
       return { success: false, message: '下载图片失败' }
     }
+    console.log('downloaded waifu2x', imageUrl)
 
     const { width, height } = await sharp(input).metadata()
-    console.log(width, height)
     if (!width || !height || width >= 4096 || height >= 4096) {
       return { success: false, message: '原图大小已经超过4K了' }
     }
@@ -53,12 +73,16 @@ new Worker(
         '-o',
         output,
       ])
-      const imgBuf = await sharp(output)
-        .resize(4096, 4096, {
-          fit: sharp.fit.inside,
-        })
-        .toFormat('jpg', { quality: 95 })
-        .toBuffer()
+      const imgBuf = await (async () => {
+        const img = sharp(output)
+        const { width, height } = await img.metadata()
+        if (width && height && (width > 4096 || height > 4096)) {
+          img.resize(4096, 4096, {
+            fit: sharp.fit.inside,
+          })
+        }
+        return img.toFormat('jpg', { quality: 95 }).toBuffer()
+      })()
       const fileKey = await uploadBuf('', imgBuf, 'waifu2x/')
 
       fs.unlinkSync(input)
