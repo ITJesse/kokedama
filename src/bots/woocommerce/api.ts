682506cab240aa3682666cfb900f1c775fe23005
Jesse Zhu <jesse@itjesse.com>
2022-05-12 02:29:59 +0900

    webhook

--- src/bots/woocommerce/api.ts
+++ src/bots/woocommerce/api.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.post('/new_order', async (req, res) => {
+  console.log(req.query)
   console.log(req.body)
   console.log(req.headers)
   res.send('ok')
