f20013ed34f3ebe4873cabb3eae35aeb3ee51b5c
Jesse Zhu <jesse@itjesse.com>
2022-05-12 03:11:38 +0900

    fix

--- src/lib/woocommerce.ts
+++ src/lib/woocommerce.ts
@@ -1,7 +1,7 @@
 import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api'
 
 export const wooApi = new WooCommerceRestApi({
-  url: process.env.NEXT_PUBLIC_SHOP_BASE_URL ?? '',
+  url: process.env.SHOP_BASE_URL ?? '',
   consumerKey: process.env.WOO_CONSUMER_KEY ?? '',
   consumerSecret: process.env.WOO_CONSUMER_SECRET ?? '',
   version: 'wc/v3',
