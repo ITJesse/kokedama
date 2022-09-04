85e6de734016aad9df18b4c2b7248d6c53dbdbbf
Jesse Zhu <jesse@itjesse.com>
2022-09-05 08:22:10 +0900

    fix video url

--- src/bots/twitter/func/downloadTweetVideo.ts
+++ src/bots/twitter/func/downloadTweetVideo.ts
@@ -18,8 +18,11 @@ export default async function downloadTweetVideo(
       (e: any) => e.type === 'video' || e.type === 'animated_gif',
     ) ?? false
   if (video) {
+    const videoUrl = tweet.includes.media[0].variants
+      .filter((e: any) => e.content_type === 'video/mp4')
+      .sort((a: any, b: any) => b.bit_rate - a.bit_rate)[0].url
     const videoBuf = await axios
-      .get(tweet.includes.media[0].variants[0].url, {
+      .get(videoUrl, {
         responseType: 'arraybuffer',
       })
       .then(({ data }) => Buffer.from(data))
